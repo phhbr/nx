00b08dd374c99d048f9813c8b4b7ece1a117a94c
phhbr <phhbr@phhbr.de>
2026-05-29 11:26:14 +0200

    fix: remove unused import of fileURLToPath in runner.ts

--- packages/migrations/src/runner.ts
+++ packages/migrations/src/runner.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { fileURLToPath } from 'url';
 import { glob } from 'glob';
 import * as semver from 'semver';
 import type { MigrationEntry, RunOptions, RunResult } from './types';
